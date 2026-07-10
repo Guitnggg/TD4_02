7a047669239e1ead24ff18aac4cdac17c0de2c53
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2026-07-10 11:26:05 +0900

    IScene.hにコメント追加

--- DirectXGame/Source/Scene/IScene.h
+++ DirectXGame/Source/Scene/IScene.h
@@ -1,22 +1,51 @@
-﻿#pragma once
+#pragma once
 
 #include <memory>
 
 #include "SceneName.h"
 
+/// <summary>
+/// 各シーンが共通して実装する処理を定義するインターフェース
+///
+/// ・初期化、更新、描画
+/// ・シーン終了判定
+/// ・次シーンの生成と現在シーン名の取得
+/// を各シーンに実装させる
+/// </summary>
 class IScene {
 public:
+	/// <summary>
+	/// 派生シーンを安全に破棄するための仮想デストラクタ
+	/// </summary>
     virtual ~IScene() = default;
 
-	virtual void Initialize() = 0;
+    /// <summary>
+    /// シーン開始時の初期化を行う
+    /// </summary>
+    virtual void Initialize() = 0;
 
-	virtual void Update() = 0;
+    /// <summary>
+    /// シーンごとの入力、状態、遷移条件を更新する
+    /// </summary>
+    virtual void Update() = 0;
 
-	virtual void Draw() = 0;
+    /// <summary>
+    /// シーンごとの描画処理を行う
+    /// </summary>
+    virtual void Draw() = 0;
 
-	virtual bool IsEnd() const = 0;
+    /// <summary>
+    /// シーンが終了して次シーンへ遷移できるか取得する
+    /// </summary>
+    virtual bool IsEnd() const = 0;
 
-	virtual std::unique_ptr<IScene> NextScene() const = 0;
+    /// <summary>
+    /// 次に遷移するシーンを生成する
+    /// </summary>
+    virtual std::unique_ptr<IScene> NextScene() const = 0;
 
-	virtual SceneName GetSceneName() const = 0;
+    /// <summary>
+    /// 現在のシーン名を取得する
+    /// </summary>
+    virtual SceneName GetSceneName() const = 0;
 };
