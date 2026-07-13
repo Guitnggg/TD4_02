175866656e02fee70f682c45f9c359ad9713d466
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2026-07-13 15:14:53 +0900

    UIのマージ完了

--- DirectXGame/Source/Scene/Title/TitleScene.cpp
+++ DirectXGame/Source/Scene/Title/TitleScene.cpp
@@ -14,27 +14,27 @@ using namespace KamataEngine;
 void TitleScene::Initialize() { isEnd_ = false; }
 
 void TitleScene::Update() {
-	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
-		isEnd_ = true;
-	}
+    if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+        isEnd_ = true;
+    }
 }
 
 void TitleScene::Draw() {
 #ifdef USE_IMGUI
-	ImGui::SetNextWindowPos(ImVec2(520.0f, 280.0f), ImGuiCond_FirstUseEver);
-	ImGui::SetNextWindowSize(ImVec2(240.0f, 120.0f), ImGuiCond_FirstUseEver);
-	ImGui::Begin("TitleScene");
-	ImGui::Text("Title Scene");
-	ImGui::Separator();
-	ImGui::Text("Press SPACE");
-	ImGui::End();
+    ImGui::SetNextWindowPos(ImVec2(520.0f, 280.0f), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(240.0f, 120.0f), ImGuiCond_FirstUseEver);
+    ImGui::Begin("TitleScene");
+    ImGui::Text("Title Scene");
+    ImGui::Separator();
+    ImGui::Text("Press SPACE");
+    ImGui::End();
 #endif
 }
 
 bool TitleScene::IsEnd() const { return isEnd_; }
 
 std::unique_ptr<IScene> TitleScene::NextScene() const {
-	return std::make_unique<DifficultySelectScene>();
+    return std::make_unique<DifficultySelectScene>();
 }
 
 SceneName TitleScene::GetSceneName() const { return SceneName::Title; }
